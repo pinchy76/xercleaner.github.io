b82667ba4b01ac88d48ed0fa9e4a4359af6ae887
Stephen Pincher <stephen.pincher@gmail.com>
2022-03-29 11:14:23 +0100

    strTagValue = document.getElementById("inputTagValue").value + "_"

--- js/xerParser.js
+++ js/xerParser.js
@@ -91,7 +91,7 @@ function xerParser() {
   const optChkClearTag = document.querySelector('input[name="taggingOption"]:checked').value;
 
   if (optChkClearTag == "Tag") {
-    const strTagValue = `${document.getElementById("inputTagValue")}_`; 
+    const strTagValue = document.getElementById("inputTagValue").value + "_"; 
     const arrTagTables = [
       [`CALENDAR`, `clndr_name`],
       [`ROLES`, `role_short_name`],
